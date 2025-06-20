6a9d0be46969ac6f7bfd648c19181e3ccd216d1b
VictorElder <eldervic001@gmail.com>
2025-06-20 11:14:27 +0100

    my first github push

---
+++ script.js
@@ -0,0 +1,44 @@
+//document.getElementById('count-el').innerText = 5
+
+// let count = 5
+
+// count =2 + count
+
+// console.log(count)
+
+ let countEl = document.getElementById('count-el')
+
+ let count = 0
+
+ function increment() {
+     count += 1
+     countEl.innerText = count
+ }
+
+ let countVar = "12" - 
+
+
+ function save() {
+     saveEl.innerText = countVar
+    console.log(count)
+ }
+// const name = "Victor";
+// const greeting = 'Hi, my name is';
+
+// const myGreeting = greeting + ' ' + name;
+// console.log(myGreeting)
+
+let welcomeEl = document.getElementById("welcome-El");
+
+let name = "Victor";
+let greeting = "Welcome back "
+
+welcomeEl.innerText = greeting + name
+
+
+
+let saveEl = document.getElementById('save-el')
+
+
+
+
